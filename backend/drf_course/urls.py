2374a26db04b337c309e5d9db2969aa23def0937
yandaki <boayves@gmail.com>
2023-09-25 18:07:44 +0000

    config docker

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
 from core import views as views_core
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter();
 
@@ -9,5 +10,6 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/' , admin.site.urls),
-    path('contact/' , views_core.ContactAPIView.as_view())
+    path('contact/' , views_core.ContactAPIView.as_view()),
+    path('api-token-auth/' , obtain_auth_token)
 ]
