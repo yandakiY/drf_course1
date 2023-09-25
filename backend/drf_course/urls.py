d347988cbe348eb44e5c804bc315c5cb7339433b
yandaki <boayves@gmail.com>
2023-09-25 11:49:43 +0000

    implementation views Contact Api

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
+from core import views as views_core
 
 router = routers.DefaultRouter();
 
 urlpatterns = router.urls
 
 urlpatterns += [
-    path('admin/' , admin.site.urls)
+    path('admin/' , admin.site.urls),
+    path('contact/' , views_core.ContactAPIView.as_view())
 ]
